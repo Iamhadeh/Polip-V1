ac5bf0497a92a8b0b0ca593187c12784f07dee8e
ADEKUSIBE OGUNMOKUN <cusibeade@gmail.com>
2024-12-28 05:12:28 +0100

    page cut out temporarily

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,15 +1,15 @@
-import React from 'react'
-import MainAbout from '../Components/AboutComponents/MainAbout'
+// import React from 'react'
+// import MainAbout from '../Components/AboutComponents/MainAbout'
 
-import '../Components/AboutComponents/About.css'
+// import '../Components/AboutComponents/About.css'
 
 
-const About = () => {
-  return (
-    <>
-    <MainAbout/>
-    </>
-  )
-}
+// const About = () => {
+//   return (
+//     <>
+//     <MainAbout/>
+//     </>
+//   )
+// }
 
-export default About;
+// export default About;
