618cdb0bd31344df7c0748501154c516d284b194
ADEKUSIBE OGUNMOKUN <cusibeade@gmail.com>
2024-12-28 05:02:46 +0100

    come on, please

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import MainAbout from '../Components/AboutComponents/MainAbout.jsx'
+import MainAbout from '../Components/AboutComponents/MainAbout'
 
 import '../Components/AboutComponents/About.css'
 
